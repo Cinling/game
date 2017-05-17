9746c8f89c4fac04d4900f965b937d092238b1b3
Cinling <1105412681@qq.com>
2017-05-18 00:37:28 +0800

    新增全局角色移动的方法

--- code/unity3d/Assets/script/ai/base_ai.cs
+++ code/unity3d/Assets/script/ai/base_ai.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class base_ai : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class base_ai : MonoBehaviour
+{
+	
+	void Start ()
+	{
 		
 	}
 	
-	// Update is called once per frame
+	/**
+	 * @version 2017年5月18日 00:23:40
+	 */
 	void Update ()
-    {
-		
+	{
+		ObjectMoveFunc.move(this.gameObject, new Vector3(0, -0.1f, 0));
 	}
 }
