ff375cf1f7f84edefc1f145ff25150f8eea6510b
Cinling <1105412681@qq.com>
2017-05-24 23:20:56 +0800

    初始化好地表的碰撞事件。目前直接使用box碰撞实现。整个地板都是平面

--- code/unity3d/Assets/script/obj/BaseAnimal.cs
+++ code/unity3d/Assets/script/obj/BaseAnimal.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 2017年5月24日 22:40:36
+/// 所有动物的基类
+/// </summary>
 public class BaseAnimal : MonoBehaviour
 {
 	public float speed;
 
+	
 	public BaseAnimal(float speed)
 	{
 		this.speed = speed;
