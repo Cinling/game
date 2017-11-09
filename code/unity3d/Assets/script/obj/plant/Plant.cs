b0c7d7bf8d7616cb2c24b49293e13928703d65d7
Cinling <1105412681@qq.com>
2017-11-10 00:21:33 +0800

    添加基本的植物生长周期架构

--- code/unity3d/Assets/script/obj/plant/Plant.cs
+++ code/unity3d/Assets/script/obj/plant/Plant.cs
@@ -3,9 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// 2017-11-09 23:55:24
+/// 植物抽象父类
+/// 
+/// 说明：
+///     生长周期：发芽 - 出芽 - 成熟 - 结果 - 死亡
+/// </summary>
 public abstract class Plant : BaseRole
 {
-	public override void Do()
+    public const byte LIFE_CYCLE_SPROUT = 0;    // 发芽（未出土）
+    public const byte LIFE_CYCLE_BUD = 1;       // 出芽
+    public const byte LIFE_CYCLE_MATURE = 2;    // 成熟
+    public const byte LIFE_CYCLE_BEAR = 3;      // 结果
+    public const byte LIFE_CYCLE_DEATH = 4;     // 死亡
+
+    protected int life_time;                    // 已生存的时间
+
+    public override void Do()
 	{
 		
 	}
@@ -16,5 +32,14 @@ public abstract class Plant : BaseRole
 	}
 
 
+    /// <summary>
+    /// 2017-11-09 23:54:19
+    /// 初始化植物
+    /// 默认是未出土
+    /// </summary>
 	protected abstract void InitPlant();
+
+
+    // 当生命状态改变
+    protected abstract void OnLifeCycleChange(byte life_cycle);
 }
--- code/unity3d/Assets/script/obj/plant/angiosperm/Carrot.cs
+++ code/unity3d/Assets/script/obj/plant/angiosperm/Carrot.cs
@@ -9,4 +9,34 @@ public class Carrot : Plant
 	{
 		Log.PrintLog("Carrot", "InitPlant", "init", Log.LOG_LEVEL.INFO);
 	}
+
+    protected override void OnLifeCycleChange(byte life_cycle)
+    {
+        switch (life_cycle)
+        {
+            // 发芽
+            case Plant.LIFE_CYCLE_SPROUT:
+                break;
+
+            // 出芽
+            case Plant.LIFE_CYCLE_BUD:
+                break;
+
+            // 成熟
+            case Plant.LIFE_CYCLE_MATURE:
+                break;
+
+            // 结果
+            case Plant.LIFE_CYCLE_BEAR:
+                break;
+
+            // 死亡
+            case Plant.LIFE_CYCLE_DEATH:
+                break;
+
+            // 未知状态
+            default:
+                break;
+        }
+    }
 }
