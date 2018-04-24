08344773f71e7d21768d4ed3ead8423efe00f715
cinling <1105412681@qq.com>
2018-04-25 00:02:33 +0800

    修改好初版的自动化打包脚本

--- code/tools/build.bat
+++ code/tools/build.bat
@@ -1,3 +1,12 @@
-REM start /wait "C:\Program Files\Unity\Editor\Unity.exe"  -quit  -batchmode   -projectPath "D:\workspace\repository\u3d\code\unity3d"  -executeMethod Build.Test  %companyName%  %productName% %platform% %outPath%  %arg0%
 
-"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -projectPath "D:\workspace\repository\u3d\code\unity3d" -nographics -executeMethod Build.Test -logFile "D:\workspace\repository\u3d\code\unity3d\bat_build.log"  -quit
+REM update git
+git pull
+
+REM packaging Unity3d
+"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -projectPath "D:\workspace\repository\u3d_package\code\unity3d" -nographics -executeMethod Build.Test -logFile "D:\workspace\repository\u3d_package\code\unity3d\bat_build.log"  -quit
+
+REM compaile C++
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "D:\workspace\repository\u3d_package\code\srv\srv\srv.vcxproj" /p:Configuration=Debug /m
+
+REM copy srv.exe tu Unity3d folder
+copy D:\workspace\repository\u3d_package\code\srv\srv\srv.exe E:\u3d_package\test\
